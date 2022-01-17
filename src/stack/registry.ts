2d5b8d085074122f042984b6a85991efc588b25a
Brett <brett@cto.ai>
2022-01-17 17:50:52 -0400

    Update stack with latest changes

--- src/stack/registry.ts
+++ src/stack/registry.ts
@@ -2,23 +2,43 @@ import cdk = require('@aws-cdk/core')
 import ecr = require('@aws-cdk/aws-ecr')
 
 interface StackProps {
+  org: string
+  env: string
   repo: string
- }
+  tag: string
+  key: string
+  entropy: string
+}
 
 export default class Registry extends cdk.Stack {
-  public readonly repo: ecr.Repository
+
+  public readonly id: string
+  public readonly org: string
+  public readonly env: string
+  public readonly repo: string
+  public readonly tag: string
+  public readonly key: string
+  public readonly entropy: string
+
+  public readonly repository: ecr.Repository
+
   constructor(scope: cdk.Construct, id: string, props?: StackProps) {
     super(scope, id)
+    this.id = id
+    this.org = props?.org ?? 'cto-ai'
+    this.env = props?.env ?? 'dev'
+    this.key = props?.key ?? 'do-k8s'
+    this.repo = props?.repo ?? 'sample-app'
+    this.tag = props?.tag ?? 'main'
+    this.entropy = props?.entropy ?? '01012022'
 
-    const repo = props?.repo ?? 'sample-app'
-
-    const repository = new ecr.Repository(this, `${repo}`, {
-      repositoryName: `${repo}`,
+    const repository = new ecr.Repository(this, `${this.repo}-${this.key}`, {
+      repositoryName: `${this.repo}-${this.key}`,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     })
 
-    this.repo = repository;
-    new cdk.CfnOutput(this, `${repo}Registry`, { value: repository.repositoryUri });
+    this.repository = repository
+    new cdk.CfnOutput(this, `${this.id}Registry`, { value: repository.repositoryUri })
   }
 }
 
