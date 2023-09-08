ac9bd47399aae47537ac94b13ab149c89c9f7344
Brett <brett@cto.ai>
2023-09-08 15:20:32 -0300

    added all to prompt env choices

--- src/prompts/index.ts
+++ src/prompts/index.ts
@@ -6,7 +6,7 @@ export function stackEnvPrompt () {
     } > ({
         type: 'list',
         name: 'STACK_ENV',
-        choices: ['dev', 'stg', 'prd'],
+        choices: ['dev', 'stg', 'prd', 'all'],
         default: 'dev',
         message: 'What is the name of the environment?',
     })
